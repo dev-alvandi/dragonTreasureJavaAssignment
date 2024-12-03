43ccf2e967013b13a71cb5d55a623e4d05fa53df
Therese <therese.danley@gmail.com>
2024-12-03 21:16:33 +0100

    added documentation comments to most files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+
+/**
+ * * Class for running the game.
+ */
+
 public class Main {
     public static void main(String[] args) {
 
         // create and initialize dungeon object
         Dungeon dungeon = new Dungeon();
-        // start game
+        // run game
         dungeon.playGame();
     }
 }
